1ed3286ef48a3a8248108dbf5b99bae9b088b2d6
sha-mayn teh <shamayn@gmail.com>
2021-12-30 23:24:00 -0500

    day18 minor cleanup

--- src/day18/index.js
+++ src/day18/index.js
@@ -1,4 +1,6 @@
-const { test, readInput } = require('../utils');
+const { test, readInput, debuglog } = require('../utils');
+
+const LOG_LEVEL = 0;
 
 const prepareInput = (rawInput) => {
   return rawInput.map((row) => {
@@ -9,52 +11,29 @@ const prepareInput = (rawInput) => {
 const input = prepareInput(readInput('input.txt'));
 const testInput = prepareInput(readInput('testinput.txt'));
 
-const calcMagnitude = (input) => {
-  if (!Array.isArray(input)) {
-    return input;
-  }
-  return 3 * calcMagnitude(input[0]) + 2 * calcMagnitude(input[1]);
-};
-
-const addandreduce = (prev, curr) => {
-  var pair = [prev, curr];
-  var result = reduce(pair);
-  return result;
-};
-
 class Node {
   constructor() {
     this.val = null;
     this.left = null;
     this.right = null;
     this.level = 0;
+    this.id = -1;
   }
 }
 
 // Helper function to allocate a new node
-// with the given data
 const newNode = (val, level) => {
     var node = new Node();
     node.val = val;
-    node.id = -1;
-    node.left = null;
-    node.right = null;
     node.level = level;
     return node;
 };
 
-const reduce = (pair) => {
-  var leaves = [];
-  var root = buildTree(pair, 0, leaves);
-  var actioncount = -1;
-  while (actioncount !== 0) {
-    actioncount = explodeReduce(root, leaves, 0);
-    if (actioncount === 0) {
-      actioncount = splitReduce(root, leaves, 0);
-    }
-    updateTreeVal(root);
-  } 
-  return root.val;
+const calcMagnitude = (input) => {
+  if (!Array.isArray(input)) {
+    return input;
+  }
+  return 3 * calcMagnitude(input[0]) + 2 * calcMagnitude(input[1]);
 };
 
 const buildTree = (pair, level, leaves) => {
@@ -83,20 +62,15 @@ const updateTreeVal = (node) => {
 };
 
 const explodeReduce = (node, leaves, actioncount) => {
-  // console.log('updateLeaves', (node === null) ? null : node.val, 'actioncount', actioncount);
-  if (node === null) {
-    return actioncount;
-  }
-  if (node.left === null && node.right === null) {
+  if (node === null || (node.left === null && node.right === null)) {
     return actioncount;
   }
   if (actioncount > 0) {
     return actioncount;
   }
   actioncount = explodeReduce(node.left, leaves, actioncount);
-  if (node.left !== null && node.right !== null &&
-    !Array.isArray(node.left.val) && !Array.isArray(node.left.val) && 
-    node.level === 4 && actioncount === 0) {
+  if (actioncount === 0 && node.level === 4 && node.left !== null && node.right !== null &&
+    !Array.isArray(node.left.val) && !Array.isArray(node.left.val)) {
       var leftval = node.left.val;
       var rightval = node.right.val;
       node.val = 0;
@@ -111,26 +85,20 @@ const explodeReduce = (node, leaves, actioncount) => {
       leaves.splice(leftindex, 2, node);
       node.left = null;
       node.right = null;
-      actioncount++;
-      return actioncount;
+      return actioncount + 1;
   }
-  actioncount = explodeReduce(node.right, leaves, actioncount);
-  node.val = [node.left.val, node.right.val];
-  return actioncount;
+  return explodeReduce(node.right, leaves, actioncount);
 };
 
 const splitReduce = (node, leaves, actioncount) => {
-  if (node === null) {
-    return actioncount;
-  }
-  if (node.left === null && node.right === null) {
+  if (node === null || (node.left === null && node.right === null)) {
     return actioncount;
   }
   if (actioncount > 0) {
     return actioncount;
   }
   actioncount = splitReduce(node.left, leaves, actioncount);
-  if ((node.left.val >= 10 || node.right.val >= 10) && actioncount === 0) {
+  if (actioncount === 0 && (node.left.val >= 10 || node.right.val >= 10)) {
       // e.g. 13 => [5, 6]
       // this node becomes a parent node
       const splitleft = (node.left.val > 9) ? true : false;
@@ -148,25 +116,31 @@ const splitReduce = (node, leaves, actioncount) => {
       } else {
         node.right = splitnode;
       }
-      node.val = [node.left.val, node.right.val];
-
       leaves.splice(splitindex, 1, splitnode.left, splitnode.right);
-      actioncount++;
-      return actioncount;
+      return actioncount + 1;
   }
-  actioncount = splitReduce(node.right, leaves, actioncount);
-  node.val = [node.left.val, node.right.val];
-  return actioncount;
+  return splitReduce(node.right, leaves, actioncount);
 };
 
-const finalSum = (input) => {
-  var result = input.reduce((prev, curr) => 
-    addandreduce(prev, curr));
-  return result;
+const addandreduce = (prev, curr) => {
+  var pair = [prev, curr];
+  var leaves = [];
+  var root = buildTree(pair, 0, leaves);
+  var actioncount = -1;
+  while (actioncount !== 0) {
+    actioncount = explodeReduce(root, leaves, 0);
+    if (actioncount === 0) {
+      actioncount = splitReduce(root, leaves, 0);
+    }
+    updateTreeVal(root);
+    // debuglog(LOG_LEVEL, JSON.stringify(root.val));
+  } 
+  return root.val;
 };
 
 const goA = (input) => {
-  return calcMagnitude(finalSum(input));
+  var finalSum = input.reduce((prev, curr) => addandreduce(prev, curr));
+  return calcMagnitude(finalSum);
 };
 
 const goB = (input) => {
@@ -187,10 +161,8 @@ const goB = (input) => {
 
 /* Tests */
 test(calcMagnitude([[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]), 3488);
-test(JSON.stringify(reduce([[[[[9,8],1],2],3],4]), 1), JSON.stringify([[[[0,9],2],3],4]));
-test(JSON.stringify(reduce([7,[6,[5,[4,[3,2]]]]])), JSON.stringify([7,[6,[5,[7,0]]]]));
-test(JSON.stringify(reduce([[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]])), JSON.stringify([[[[0,7],4],[[7,8],[6,0]]],[8,1]]));
-test(JSON.stringify(reduce([[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]],[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]])), JSON.stringify([[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]));
+test(JSON.stringify(addandreduce([[[[4,3],4],4],[7,[[8,4],9]]],[1,1])), JSON.stringify([[[[0,7],4],[[7,8],[6,0]]],[8,1]]));
+test(JSON.stringify(addandreduce([[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]],[7,[[[3,7],[4,3]],[[6,3],[8,8]]]])), JSON.stringify([[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]));
 test(goA(testInput), 4140);
 test(goB(testInput), 3993);
 
